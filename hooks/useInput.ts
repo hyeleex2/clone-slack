4a40d596744fc5f3d0914752636c022f44b6eab4
hl.lee@quintet.co.kr <hyeleex2@gmail.com>
2023-05-20 15:41:36 +0900

    first commit

---
+++ hooks/useInput.ts
@@ -0,0 +1,11 @@
+import { ChangeEvent, Dispatch, SetStateAction, useCallback, useState } from "react";
+
+const useInput = <T>(initialData: T): [T, (e: ChangeEvent<HTMLInputElement>) => void, Dispatch<SetStateAction<T>>] => {
+  const [value, setValue] = useState(initialData)
+  const handler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value as T)
+  }, [])
+  return [value, handler, setValue]
+}
+
+export default useInput
---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import "./App.css";
+import loadable from "@loadable/component";
+import Login from "@pages/Login";
+import SignUp from "@pages/SignUp";
+
+// const Login = loadable(() => import("@pages/Login"));
+// const SignUp = loadable(() => import("@pages/SignUp"));
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/login" />} />
+        <Route path="/login" Component={Login} />
+        <Route path="/signup" Component={SignUp} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
